8e51eda0cc736d0d157ce49a44b3ef2a4867054e
dctagacbfen <zxz442964605123@163.com>
2021-04-12 14:10:16 +0800

    删除目录app

--- app/src/main/java/com/example/a14110100202/yuejian_29/activity/ClubListActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.a14110100202.yuejian_29.activity;
-
-/**
- * Created by HP on 2017/9/25.
- */
-
-public class ClubListActivity {
-}
--- app/src/main/java/com/example/a14110100202/yuejian_29/fragement/ClubVideoFragement.java
+++
@@ -1,8 +0,0 @@
-package com.example.a14110100202.yuejian_29.fragement;
-
-/**
- * Created by HP on 2017/9/25.
- */
-
-public class ClubVideoFragement {
-}
